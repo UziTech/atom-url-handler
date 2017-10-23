f25ab411edee060ed6f206d2c8697db7a2b04dca
Tony Brix <tony@brix.ninja>
2017-10-23 16:44:20 -0500

    choose protocol on install

--- install.bat
+++ install.bat
@@ -1,16 +1,25 @@
 @echo off
 
 :: request admin privileges
-if not "%1"=="is_admin" (powershell start -verb runas '%0' is_admin & exit)
+if not "%1"=="is_admin" (
+	powershell start -verb runas '%0' is_admin
+	exit
+)
+
+:: choose protocol
+set /P prot=Protocol [atm]? 
+if "%prot%" == "" (
+	set prot=atm
+)
 
 :: copy handler
 copy /Y "%~dp0atom-url-handler.bat" "%LOCALAPPDATA%\atom-url-handler.bat"
 
 :: add registry keys
-reg add HKCR\atm /f
-reg add HKCR\atm /ve /d "URL:Open Atom" /f
-reg add HKCR\atm /v "URL Protocol" /d "" /f
-reg add HKCR\atm\shell /f
-reg add HKCR\atm\shell\open /f
-reg add HKCR\atm\shell\open\command /f
-reg add HKCR\atm\shell\open\command /ve /d "\"%LOCALAPPDATA%\atom-url-handler.bat\" \"%%1\"" /f
+reg add HKCR\%prot% /f
+reg add HKCR\%prot% /ve /d "URL:Open Atom" /f
+reg add HKCR\%prot% /v "URL Protocol" /d "" /f
+reg add HKCR\%prot%\shell /f
+reg add HKCR\%prot%\shell\open /f
+reg add HKCR\%prot%\shell\open\command /f
+reg add HKCR\%prot%\shell\open\command /ve /d "\"%LOCALAPPDATA%\atom-url-handler.bat\" \"%%1\"" /f
