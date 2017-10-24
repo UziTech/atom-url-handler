54f42990f962141cecee619d030d961b535a4289
Tony Brix <tony@brix.ninja>
2017-10-23 23:44:14 -0500

    allow protocol to be first argument

--- install.bat
+++ install.bat
@@ -1,9 +1,14 @@
 @echo off
 
 :: request admin privileges
-if not "%1"=="is_admin" (
-	powershell start -verb runas '%0' is_admin
+if not "%~1"=="is_admin" (
+	powershell start -verb runas '%0' 'is_admin "%~1"'
 	exit
+)
+
+if not "%~2" == "" (
+	set "prot=%~2"
+	goto start
 )
 
 :: choose protocol
@@ -11,6 +16,10 @@ set /P prot=Protocol [atm]?
 if "%prot%" == "" (
 	set prot=atm
 )
+
+:start
+echo "%prot%"
+pause
 
 :: copy handler
 copy /Y "%~dp0atom-url-handler.bat" "%LOCALAPPDATA%\atom-url-handler.bat"
